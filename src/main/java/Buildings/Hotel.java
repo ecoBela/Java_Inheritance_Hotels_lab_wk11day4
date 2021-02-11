9952c4a35184606c7921303699f7517d288baefe
Bela <belahamid@gmail.com>
2021-02-11 12:02:31 +0000

    hotel has bedrooms - test passed

--- src/main/java/Buildings/Hotel.java
+++ src/main/java/Buildings/Hotel.java
@@ -1,4 +1,24 @@
 package Buildings;
 
+import Rooms.Bedroom;
+import Rooms.ConferenceRoom;
+
+import java.util.ArrayList;
+
 public class Hotel {
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    public Hotel(ArrayList<Bedroom> bedrooms, ArrayList<ConferenceRoom> conferenceRooms){
+        this.bedrooms = bedrooms;
+        this.conferenceRooms = conferenceRooms;
+    }
+
+    public int getBedrooms() {
+        return bedrooms.size();
+    }
+
+    public ArrayList<ConferenceRoom> getConferenceRooms() {
+        return conferenceRooms;
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,44 @@
+import Buildings.Hotel;
+import Rooms.Bedroom;
+import Rooms.ConferenceRoom;
+import Rooms.RoomType;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+    Hotel hotel;
+    Bedroom bedroom1;
+    Bedroom bedroom2;
+    ConferenceRoom conferenceRoom1;
+    ConferenceRoom conferenceRoom2;
+    ArrayList<Bedroom> bedrooms;
+    ArrayList<ConferenceRoom> conferenceRooms;
+
+
+    @Before
+    public void before(){
+        bedroom1 = new Bedroom(2, 44, RoomType.DOUBLE);
+        bedroom2 = new Bedroom(3, 77, RoomType.TRIPLE);
+        conferenceRoom1 = new ConferenceRoom(100, "Peppermint");
+        conferenceRoom2 = new ConferenceRoom(120, "Spearmint");
+        bedrooms = new ArrayList<Bedroom>();
+        conferenceRooms = new ArrayList<ConferenceRoom>();
+        
+        bedrooms.add(bedroom1);
+        bedrooms.add(bedroom2);
+        conferenceRooms.add(conferenceRoom1);
+        conferenceRooms.add(conferenceRoom2);
+        hotel = new Hotel(bedrooms, conferenceRooms);
+
+    }
+
+    @Test
+    public void hasBedrooms(){
+        assertEquals(2, hotel.getBedrooms());
+    }
+
 }
