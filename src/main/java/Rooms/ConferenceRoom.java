36a922b62cd6faadb17ae40f07fe732c94713718
Bela <belahamid@gmail.com>
2021-02-11 11:28:08 +0000

    all tests passed for Conference Room

--- src/main/java/Rooms/ConferenceRoom.java
+++ src/main/java/Rooms/ConferenceRoom.java
@@ -1,4 +1,14 @@
 package Rooms;
 
-public class ConferenceRoom {
+public class ConferenceRoom extends Room{
+    private String name;
+
+    public ConferenceRoom(int capacity, String name){
+        super(capacity);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,29 @@
+import Rooms.ConferenceRoom;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+    ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before(){
+        conferenceRoom = new ConferenceRoom(100, "Bubbles");
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(100, conferenceRoom.getCapacity());
+    }
+
+    @Test
+    public void hasGuests(){
+        assertEquals(0, conferenceRoom.getGuests());
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Bubbles", conferenceRoom.getName());
+    }
 }
